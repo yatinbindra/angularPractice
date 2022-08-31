4fec895e72b1908a791ddd1e67d2d78fa5c4c812
Hament Bindra <hamentbindra12@gmail.com>
2022-08-31 11:51:47 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app1';
+  a:number=0;
+  i:number=0;
+  imageNo=1;
+  imagePath:any="";
+  abc(){
+    
+    setInterval(()=>{this.page()},1000); 
+   
+   
+    }
+page(){
+  this.a++;
+  if(this.a==5){
+
+  if(this.imageNo<5){
+    this.imageNo++;
+  }
+ else{
+  this.imageNo=1;
+ }
+
+  
+this.imagePath="assets/app1/img"+this.imageNo+".jpg"
+this.a=0;
+}
+
+}
+
 }
